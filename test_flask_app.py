78ba8689a2da12a308d5e43f236ad9a4131c9194
pharrukh <farrukh.normuradov@gmail.com>
2021-04-07 18:21:53 +0200

    test: cover all endpoints

--- test_flask_app.py
+++ test_flask_app.py
@@ -35,6 +35,55 @@ class BasicTests(unittest.TestCase):
  
     def test_get_categories(self):
         response = self.app.get('/categories')
+        json_data = response.get_json()
+
+        self.assertIsNotNone(json_data['categories'])
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_questions(self):
+        response = self.app.get('/questions')
+        json_data = response.get_json()
+
+        self.assertIsNotNone(json_data['questions'])
+        self.assertIsNotNone(json_data['total_questions'])
+        self.assertIsNotNone(json_data['categories'])
+        self.assertIsNotNone(json_data['current_category'])
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_questions_search(self):
+        response = self.app.post('/questions', json={'searchTerm': 'president'})
+        json_data = response.get_json()
+
+        self.assertIsNotNone(json_data['questions'])
+        self.assertIsNotNone(json_data['total_questions'])
+        self.assertIsNotNone(json_data['current_category'])
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_questions_create(self):
+        response = self.app.post('/questions', json={'id': 99, 'question':'Where is Uzbekistan located?', 'answer':'Central Asia', 'category':'geography'})
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_questions(self):
+        response = self.app.delete('/questions/2')
+        self.assertEqual(response.status_code, 204)
+
+    def test_get_questions_by_category(self):
+        response = self.app.get('/categories/history/questions')
+        json_data = response.get_json()
+
+        self.assertIsNotNone(json_data['questions'])
+        self.assertIsNotNone(json_data['total_questions'])
+        self.assertIsNotNone(json_data['current_category'])
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_quizzes(self):
+        response = self.app.post('/quizzes', json={'quiz_category': 'history', 'previous_questions': []})
+        json_data = response.get_json()
+
+        self.assertIsNotNone(json_data['question'])
         self.assertEqual(response.status_code, 200)
  
  
