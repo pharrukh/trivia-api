f69f44dcb1402dcc92ea7e9ed7483a890001df7f
pharrukh <farrukh.normuradov@gmail.com>
2021-04-07 14:21:39 +0200

    refactor: extract static data

---
+++ _data/__init__.py
@@ -0,0 +1,170 @@
+categories_coll = {
+    'geography': 'geography',
+    'history': 'history',
+    'science': 'science'
+}
+
+
+questions_coll = [
+    {
+        'id': 1,
+        'question': 'Who is the author of the anthem of Uzbekistan?',
+        'difficulty': 3,
+        'answer': 'Abdulla Aripov',
+        'category': 'geography'
+    },
+    {
+        'id': 2,
+        'question': 'Who is the first president of Uzbekistan?',
+        'difficulty': 2,
+        'answer': 'Islam Karimov',
+        'category': 'geography'
+    },
+    {
+        'id': 3,
+        'question': 'On what year did Uzbekistan declare independence?',
+        'difficulty': 3,
+        'answer': '1991',
+        'category': 'geography'
+    },
+    {
+        'id': 4,
+        'question': 'What is the name of the second president of Uzbekistan?',
+        'difficulty': 1,
+        'answer': 'Shavkat Mirziyaev',
+        'category': 'geography'
+    },
+    {
+        'id': 5,
+        'question': 'What is the northen neighbor of Uzbekistan?',
+        'difficulty': 4,
+        'answer': 'Kazakhstan',
+        'category': 'geography'
+    },
+    {
+        'id': 6,
+        'question': 'Is population of Uzbeksistan larger than 30 million?',
+        'difficulty': 1,
+        'answer': 'Yes',
+        'category': 'geography'
+    },
+    {
+        'id': 7,
+        'question': 'To whom does this quote belong: "I prefer a short life with width to a narrow one with length."',
+        'difficulty': 5,
+        'answer': 'Avicenna',
+        'category': 'science'
+    },
+    {
+        'id': 8,
+        'question': 'To whom does this quote belong: "My own soul is my most faithful friend. My own heart, my trust confidant."',
+        'difficulty': 5,
+        'answer': 'Babur',
+        'category': 'science'
+    },
+    {
+        'id': 9,
+        'question': 'Who is the national hero is Uzbekistan?',
+        'difficulty': 2,
+        'answer': 'Amir Temur',
+        'category': 'history'
+    },
+    {
+        'id': 10,
+        'question': 'Whot was the capital of Amir Timur\'s country?',
+        'difficulty': 4,
+        'answer': 'Samarkand',
+        'category': 'history'
+    },
+    {
+        'id': 11,
+        'question': 'Who is the greatest grandson of Amir Timur?',
+        'difficulty': 2,
+        'answer': 'Ulugh Bek',
+        'category': 'history'
+    },
+    {
+        'id': 12,
+        'question': 'Who was Ulugh Bek\'s father?',
+        'difficulty': 4,
+        'answer': 'Shakhrukh',
+        'category': 'history'
+    },
+    {
+        'id': 13,
+        'question': 'How did arabs call the territory above Amu Darya?',
+        'difficulty': 4,
+        'answer': 'Mawarannahr',
+        'category': 'history'
+    },
+    {
+        'id': 15,
+        'question': 'The name of what city is "rich settlement"?',
+        'difficulty': 5,
+        'answer': 'Samarkand',
+        'category': 'history'
+    },
+    {
+        'id': 16,
+        'question': 'Who is the most known poet of Timurid period?',
+        'difficulty': 3,
+        'answer': 'Ali Shir Nava\'i',
+        'category': 'history'
+    },
+    {
+        'id': 17,
+        'question': 'What language was used in Timurid empire?',
+        'difficulty': 5,
+        'answer': 'Chagatai',
+        'category': 'history'
+    },
+    {
+        'id': 18,
+        'question': 'Which dynasty brought "Uzbek" name to Timurid nations?',
+        'difficulty': 4,
+        'answer': 'Shaybanid',
+        'category': 'history'
+    },
+    {
+        'id': 19,
+        'question': 'In what year did mongols besiege Samarkand?',
+        'difficulty': 4,
+        'answer': '1220',
+        'category': 'history'
+    },
+    {
+        'id': 20,
+        'question': 'What was the name of the Kharesm emperor that oppossed Ghengis Khan?',
+        'difficulty': 4,
+        'answer': 'Mukhammad',
+        'category': 'history'
+    },
+    {
+        'id': 21,
+        'question': 'What name did Vyatkin give to the ruins that were left by mongols?',
+        'difficulty': 2,
+        'answer': 'Afrosiab',
+        'category': 'history'
+    },
+    {
+        'id': 22,
+        'question': 'What was the main tool of Ulug Bek\'s observatory?',
+        'difficulty': 4,
+        'answer': 'Sekstant',
+        'category': 'history'
+    },
+    {
+        'id': 23,
+        'question': 'Is it true that Uzbek and Turkish belong to the same language family?',
+        'difficulty': 3,
+        'answer': 'Yes',
+        'category': 'history'
+    },
+    {
+        'id': 24,
+        'question': 'Is it true that many people in Samarkand speak Tajik?',
+        'difficulty': 1,
+        'answer': 'Yes',
+        'category': 'history'
+    }
+]
